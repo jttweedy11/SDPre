1209b28ab6dcb532fb2b90b6a5ff7577708eb589
Jacob Tweedy <jttweedy11@gmail.com>
2021-07-10 17:33:23 -0500

    add objective2 labs

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jacob Tweedy";
+    int age = 32;
+    String state = "Iowa";
+
+      System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}

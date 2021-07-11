ca3d27472034b1802ec1a2eeda1382f1d0f11a08
Jacob Tweedy <jttweedy11@gmail.com>
2021-07-10 23:18:44 -0500

    commit objective 7 work

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int sumTotal = 0;
+    while(counter<=20) {
+      sumTotal = sumTotal + counter;
+      counter = counter + 1;
+    }
+    System.out.println(sumTotal);
+  }
+}
